e9875c50307a8d649a78e49620d670416a18356c
KerenArias-2001 <kearias79@misena.edu.co>
2021-08-26 13:50:03 -0500

    primer cargue

---
+++ productos/vista.php
@@ -0,0 +1,190 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Productos</title>
+
+    <!-- estilos bootstrap -->
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+
+    <!-- Estilos -->
+    <link rel="stylesheet" href="../css/vproductos.css">
+</head>
+<body>
+    
+</body>
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <div class="container">
+      <a class="navbar-brand" href="../inicio.php">Hackaton</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item ">
+            <a class="nav-link" href="../inicio.php">Inicio
+              <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../clientes/administracion.html">Clientes</a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Productos</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Cuenta</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <!-- Page Content -->
+  <div class="container">
+
+    <div class="row justify-content-md-center">
+
+      <div class="columnn">
+
+        <h1 class="text-center">Categorias</h1>
+        <div class="d-flex justify-content-center">
+          <a href="#" class="list-group-item">Micro procesadores</a>
+          <a href="#" class="list-group-item">Discos solidos SSD</a>
+          <a href="#" class="list-group-item">Placas de video</a>
+        </div>
+        <br>
+
+      </div>
+      <!-- /.col-lg-3 -->
+
+      <div class="column">
+
+
+
+        <div class="row">
+
+          <div class="col-lg-4 col-md-6 mb-4">
+            <div class="card h-100">
+              <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+              <div class="card-body">
+                <h4 class="card-title">
+                  <a href="#">Item One</a>
+                </h4>
+                <h5>$24.99</h5>
+                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
+              </div>
+              <div class="card-footer">
+                <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 mb-4">
+            <div class="card h-100">
+              <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+              <div class="card-body">
+                <h4 class="card-title">
+                  <a href="#">Item Two</a>
+                </h4>
+                <h5>$24.99</h5>
+                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur! Lorem ipsum dolor sit amet.</p>
+              </div>
+              <div class="card-footer">
+                <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 mb-4">
+            <div class="card h-100">
+              <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+              <div class="card-body">
+                <h4 class="card-title">
+                  <a href="#">Item Three</a>
+                </h4>
+                <h5>$24.99</h5>
+                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
+              </div>
+              <div class="card-footer">
+                <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 mb-4">
+            <div class="card h-100">
+              <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+              <div class="card-body">
+                <h4 class="card-title">
+                  <a href="#">Item Four</a>
+                </h4>
+                <h5>$24.99</h5>
+                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
+              </div>
+              <div class="card-footer">
+                <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 mb-4">
+            <div class="card h-100">
+              <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+              <div class="card-body">
+                <h4 class="card-title">
+                  <a href="#">Item Five</a>
+                </h4>
+                <h5>$24.99</h5>
+                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur! Lorem ipsum dolor sit amet.</p>
+              </div>
+              <div class="card-footer">
+                <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 mb-4">
+            <div class="card h-100">
+              <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+              <div class="card-body">
+                <h4 class="card-title">
+                  <a href="#">Item Six</a>
+                </h4>
+                <h5>$24.99</h5>
+                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
+              </div>
+              <div class="card-footer">
+                <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+              </div>
+            </div>
+          </div>
+
+        </div>
+        <!-- /.row -->
+
+      </div>
+      <!-- /.col-lg-9 -->
+
+    </div>
+    <!-- /.row -->
+
+  </div>
+  <!-- /.container -->
+
+  <!-- Footer -->
+  <footer class="py-5 bg-dark">
+    <div class="container">
+      <p class="m-0 text-center text-white">Copyright &copy; Your Website 2020</p>
+    </div>
+    <!-- /.container -->
+  </footer>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+</html>
---
+++ vistas/bd_multitenant-.sql
@@ -0,0 +1,173 @@
+/*
+SQLyog Ultimate v11.11 (64 bit)
+MySQL - 5.5.5-10.4.14-MariaDB : Database - bd_multitenant
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`bd_multitenant` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+
+USE `bd_multitenant`;
+
+/*Table structure for table `bodega` */
+
+DROP TABLE IF EXISTS `bodega`;
+
+CREATE TABLE `bodega` (
+  `id_bodega` int(11) NOT NULL AUTO_INCREMENT,
+  `nit_bod` int(20) NOT NULL,
+  `nombre_bod` char(30) DEFAULT NULL,
+  `direccion_bod` char(30) DEFAULT NULL,
+  `proveedor` int(20) DEFAULT NULL,
+  PRIMARY KEY (`id_bodega`,`nit_bod`),
+  KEY `fk_proveedor` (`proveedor`),
+  CONSTRAINT `fk_proveedor` FOREIGN KEY (`proveedor`) REFERENCES `proveedores` (`id_prov`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `bodega` */
+
+LOCK TABLES `bodega` WRITE;
+
+UNLOCK TABLES;
+
+/*Table structure for table `facturacion` */
+
+DROP TABLE IF EXISTS `facturacion`;
+
+CREATE TABLE `facturacion` (
+  `id_fac` int(11) NOT NULL AUTO_INCREMENT,
+  `nit_loc` int(11) NOT NULL,
+  `fecha_fac` datetime DEFAULT NULL,
+  `nombre_empleado` char(30) DEFAULT NULL,
+  `nombre_clien` char(30) DEFAULT NULL,
+  `valor_iva` decimal(10,0) DEFAULT NULL,
+  `valor_total` decimal(10,0) DEFAULT NULL,
+  `nombre_produc` char(30) DEFAULT NULL,
+  `cantidad_produc` char(30) DEFAULT NULL,
+  `facturas` int(20) DEFAULT NULL,
+  PRIMARY KEY (`id_fac`,`nit_loc`),
+  KEY `fk_facturas` (`facturas`),
+  CONSTRAINT `fk_facturas` FOREIGN KEY (`facturas`) REFERENCES `facturas` (`id_factura`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `facturacion` */
+
+LOCK TABLES `facturacion` WRITE;
+
+UNLOCK TABLES;
+
+/*Table structure for table `facturas` */
+
+DROP TABLE IF EXISTS `facturas`;
+
+CREATE TABLE `facturas` (
+  `id_factura` int(11) NOT NULL AUTO_INCREMENT,
+  `num_factura` int(11) NOT NULL,
+  `nombre_client` char(30) DEFAULT NULL,
+  `sucursal` int(20) DEFAULT NULL,
+  PRIMARY KEY (`id_factura`,`num_factura`),
+  KEY `fk_sucursal` (`sucursal`),
+  CONSTRAINT `fk_sucursal` FOREIGN KEY (`sucursal`) REFERENCES `sucursales` (`id_sucursal`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `facturas` */
+
+LOCK TABLES `facturas` WRITE;
+
+UNLOCK TABLES;
+
+/*Table structure for table `local` */
+
+DROP TABLE IF EXISTS `local`;
+
+CREATE TABLE `local` (
+  `id_local` int(11) NOT NULL AUTO_INCREMENT,
+  `nit_local` int(30) NOT NULL,
+  `nombre_local` char(30) DEFAULT NULL,
+  `tipo_local` char(30) DEFAULT NULL,
+  PRIMARY KEY (`id_local`,`nit_local`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `local` */
+
+LOCK TABLES `local` WRITE;
+
+UNLOCK TABLES;
+
+/*Table structure for table `proveedores` */
+
+DROP TABLE IF EXISTS `proveedores`;
+
+CREATE TABLE `proveedores` (
+  `id_prov` int(11) NOT NULL AUTO_INCREMENT,
+  `nit_prov` int(20) NOT NULL,
+  `nombre_prov` char(30) DEFAULT NULL,
+  `direccion_prov` char(30) DEFAULT NULL,
+  PRIMARY KEY (`id_prov`,`nit_prov`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `proveedores` */
+
+LOCK TABLES `proveedores` WRITE;
+
+UNLOCK TABLES;
+
+/*Table structure for table `sucursales` */
+
+DROP TABLE IF EXISTS `sucursales`;
+
+CREATE TABLE `sucursales` (
+  `id_sucursal` int(11) NOT NULL AUTO_INCREMENT,
+  `nit_sucursal` char(20) DEFAULT NULL,
+  `nombre_sucursal` char(30) DEFAULT NULL,
+  `direccion_sucursal` char(30) DEFAULT NULL,
+  `numero_factura` int(20) DEFAULT NULL,
+  `bodegas` int(20) DEFAULT NULL,
+  `locales` int(20) DEFAULT NULL,
+  PRIMARY KEY (`id_sucursal`),
+  KEY `fk_bodegas` (`bodegas`),
+  KEY `fk_locales` (`locales`),
+  CONSTRAINT `fk_bodegas` FOREIGN KEY (`bodegas`) REFERENCES `bodega` (`id_bodega`),
+  CONSTRAINT `fk_locales` FOREIGN KEY (`locales`) REFERENCES `local` (`id_local`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `sucursales` */
+
+LOCK TABLES `sucursales` WRITE;
+
+UNLOCK TABLES;
+
+/*Table structure for table `usuarios` */
+
+DROP TABLE IF EXISTS `usuarios`;
+
+CREATE TABLE `usuarios` (
+  `id_usu` int(11) NOT NULL AUTO_INCREMENT,
+  `num_doc_usu` char(15) NOT NULL,
+  `nombres_usu` char(30) DEFAULT NULL,
+  `direccion_usu` char(30) DEFAULT NULL,
+  `tipo_cliente` char(14) DEFAULT NULL,
+  `tipo_usuario` char(15) DEFAULT NULL,
+  `local` int(20) DEFAULT NULL,
+  PRIMARY KEY (`id_usu`,`num_doc_usu`),
+  KEY `fk_local` (`local`),
+  CONSTRAINT `fk_local` FOREIGN KEY (`local`) REFERENCES `local` (`id_local`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `usuarios` */
+
+LOCK TABLES `usuarios` WRITE;
+
+UNLOCK TABLES;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;

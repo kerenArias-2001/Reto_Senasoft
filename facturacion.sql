b622b859e575856f091f6bd9e7ec8a512e26c297
kerenArias-2001 <82288880+kerenArias-2001@users.noreply.github.com>
2021-08-26 14:13:24 -0500

    Update facturacion.sql

--- facturacion.sql
+++ facturacion.sql
@@ -1,14 +1,42 @@
 /*
-SQLyog - Free MySQL GUI v5.02
-Host - 5.5.5-10.4.6-MariaDB : Database - facturacion
+SQLyog Ultimate v11.11 (64 bit)
+MySQL - 5.5.5-10.4.14-MariaDB : Database - facturacion
 *********************************************************************
-Server version : 5.5.5-10.4.6-MariaDB
 */
 
 
-create database if not exists `facturacion`;
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`facturacion` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
 
 USE `facturacion`;
+
+/*Table structure for table `bodega` */
+
+DROP TABLE IF EXISTS `bodega`;
+
+CREATE TABLE `bodega` (
+  `id_bodega` int(20) NOT NULL AUTO_INCREMENT,
+  `direccion_bodega` char(30) DEFAULT NULL,
+  `telefono_bodega` char(20) DEFAULT NULL,
+  `codigo_product` char(20) DEFAULT NULL,
+  `cantitad_product` int(100) DEFAULT NULL,
+  `nombre_product` char(30) DEFAULT NULL,
+  PRIMARY KEY (`id_bodega`),
+  KEY `fk_cod_product` (`codigo_product`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `bodega` */
+
+LOCK TABLES `bodega` WRITE;
+
+UNLOCK TABLES;
 
 /*Table structure for table `categorias` */
 
@@ -18,17 +46,13 @@ CREATE TABLE `categorias` (
   `codigo` int(11) NOT NULL AUTO_INCREMENT,
   `descripcion` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`codigo`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 /*Data for the table `categorias` */
 
-insert into `categorias` values 
+LOCK TABLES `categorias` WRITE;
 
-(1,'Microprocesadores'),
-
-(2,'Discos Solidos SSD'),
-
-(3,'Placas De Video');
+UNLOCK TABLES;
 
 /*Table structure for table `clientes` */
 
@@ -41,17 +65,13 @@ CREATE TABLE `clientes` (
   `mail` varchar(70) DEFAULT NULL,
   `direccion` varchar(70) DEFAULT NULL,
   PRIMARY KEY (`codigo`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 
 /*Data for the table `clientes` */
 
-insert into `clientes` values 
+LOCK TABLES `clientes` WRITE;
 
-(1,'Diego Moisset','23567892','diegomoisset@gmail.com','Colon 345'),
-
-(2,'Ana Paula','643456644','anapaula@gmail.com','Dean Funes 345'),
-
-(3,'Marcos Rodriguez','65984384','marcosrodriguez@gmail.com','23 de septiembre 3444');
+UNLOCK TABLES;
 
 /*Table structure for table `detallefactura` */
 
@@ -63,46 +83,17 @@ CREATE TABLE `detallefactura` (
   `codigoproducto` int(11) DEFAULT NULL,
   `precio` float DEFAULT NULL,
   `cantidad` int(11) DEFAULT NULL,
-  PRIMARY KEY (`codigo`)
-) ENGINE=InnoDB AUTO_INCREMENT=36 DEFAULT CHARSET=latin1;
+  `factura` int(11) DEFAULT NULL,
+  PRIMARY KEY (`codigo`),
+  KEY `fk_factura` (`factura`),
+  CONSTRAINT `fk_factura` FOREIGN KEY (`factura`) REFERENCES `facturas` (`codigo`)
+) ENGINE=InnoDB AUTO_INCREMENT=44 DEFAULT CHARSET=latin1;
 
 /*Data for the table `detallefactura` */
 
-insert into `detallefactura` values 
+LOCK TABLES `detallefactura` WRITE;
 
-(1,1,3,17800,1),
-
-(2,1,6,2199,1),
-
-(3,1,8,30000,1),
-
-(4,2,7,20995,1),
-
-(5,3,1,3499,1),
-
-(6,3,2,6245,1),
-
-(7,3,3,17800,1),
-
-(8,3,4,23999,1),
-
-(9,3,5,1595,1),
-
-(10,3,6,2199,1),
-
-(11,3,7,20995,1),
-
-(12,3,8,30000,3),
-
-(15,5,1,3499,1),
-
-(16,8,1,3499,1),
-
-(33,13,8,30000,1),
-
-(34,13,6,2199,1),
-
-(35,13,4,23999,1);
+UNLOCK TABLES;
 
 /*Table structure for table `facturas` */
 
@@ -112,34 +103,16 @@ CREATE TABLE `facturas` (
   `codigo` int(11) NOT NULL AUTO_INCREMENT,
   `fecha` date DEFAULT NULL,
   `codigocliente` int(11) DEFAULT NULL,
-  PRIMARY KEY (`codigo`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`codigo`),
+  KEY `fk_cliente` (`codigocliente`),
+  CONSTRAINT `fk_cliente` FOREIGN KEY (`codigocliente`) REFERENCES `clientes` (`codigo`)
+) ENGINE=InnoDB AUTO_INCREMENT=51 DEFAULT CHARSET=latin1;
 
 /*Data for the table `facturas` */
 
-insert into `facturas` values 
+LOCK TABLES `facturas` WRITE;
 
-(1,'2019-08-28',1),
-
-(2,'2019-08-28',2),
-
-(3,'2019-08-28',3),
-
-(5,NULL,NULL),
-
-(6,NULL,NULL),
-
-(7,NULL,NULL),
-
-(8,NULL,NULL),
-
-(10,NULL,NULL),
-
-(11,'2019-08-28',1),
-
-(12,NULL,NULL),
-
-(13,'2019-08-29',1);
+UNLOCK TABLES;
 
 /*Table structure for table `productos` */
 
@@ -150,25 +123,69 @@ CREATE TABLE `productos` (
   `descripcion` varchar(50) DEFAULT NULL,
   `precio` float DEFAULT NULL,
   `codigocategoria` int(11) DEFAULT NULL,
-  PRIMARY KEY (`codigo`)
+  `bodega` int(11) DEFAULT NULL,
+  `proveedor` int(20) DEFAULT NULL,
+  PRIMARY KEY (`codigo`),
+  KEY `fk_categoria` (`codigocategoria`),
+  KEY `fk_bodeg` (`bodega`),
+  KEY `fk_proveedor` (`proveedor`),
+  CONSTRAINT `fk_bodeg` FOREIGN KEY (`bodega`) REFERENCES `bodega` (`id_bodega`),
+  CONSTRAINT `fk_categoria` FOREIGN KEY (`codigocategoria`) REFERENCES `categorias` (`codigo`),
+  CONSTRAINT `fk_proveedor` FOREIGN KEY (`proveedor`) REFERENCES `proveedor` (`id_prov`)
 ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 
 /*Data for the table `productos` */
 
-insert into `productos` values 
+LOCK TABLES `productos` WRITE;
 
-(1,'Intel celeron g4900 2/2 3.1ghz',3499,1),
+UNLOCK TABLES;
 
-(2,'Intel core i3 9100f coffeelake',6245,1),
+/*Table structure for table `proveedor` */
 
-(3,'Intel core i5 9600k',17800,1),
+DROP TABLE IF EXISTS `proveedor`;
 
-(4,'Intel core i7 8700 coffee lake',23999,1),
+CREATE TABLE `proveedor` (
+  `id_prov` int(50) NOT NULL,
+  `numdoc_prov` char(20) DEFAULT NULL,
+  `nombres_prov` char(30) DEFAULT NULL,
+  `apellidos_prov` char(30) DEFAULT NULL,
+  `nom_comercial_prov` char(30) DEFAULT NULL,
+  `ciudad_prov` char(30) DEFAULT NULL,
+  `telefono_prov` char(15) DEFAULT NULL,
+  PRIMARY KEY (`id_prov`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-(5,'ssd 120gb wd green',1595,2),
+/*Data for the table `proveedor` */
 
-(6,'ssd 240gb wd green sata iii 2.5',2199,2),
+LOCK TABLES `proveedor` WRITE;
 
-(7,'nvidia geforce gtx 1660ti',20995,3),
+UNLOCK TABLES;
 
-(8,'nvidia geforce rtx 2060 6gb',30000,3);
+/*Table structure for table `sucursales` */
+
+DROP TABLE IF EXISTS `sucursales`;
+
+CREATE TABLE `sucursales` (
+  `id_sucursal` int(11) NOT NULL AUTO_INCREMENT,
+  `nit_sucursal` char(20) NOT NULL,
+  `nombre_sucursal` char(30) DEFAULT NULL,
+  `direccion_sucursal` char(30) DEFAULT NULL,
+  `numero_factura` int(20) DEFAULT NULL,
+  `cod_bodeg` int(20) DEFAULT NULL,
+  PRIMARY KEY (`id_sucursal`,`nit_sucursal`),
+  KEY `fk_num_factu` (`numero_factura`),
+  KEY `fk_cod_bodeg` (`cod_bodeg`),
+  CONSTRAINT `fk_cod_bodeg` FOREIGN KEY (`cod_bodeg`) REFERENCES `bodega` (`id_bodega`),
+  CONSTRAINT `fk_num_factu` FOREIGN KEY (`numero_factura`) REFERENCES `facturas` (`codigo`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `sucursales` */
+
+LOCK TABLES `sucursales` WRITE;
+
+UNLOCK TABLES;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
